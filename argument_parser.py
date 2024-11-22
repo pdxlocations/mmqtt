94636cc0a9cb1bc31b1eaa2baa117b290038c391
pdxlocations <benlipsey@gmail.com>
2024-11-22 13:08:34 -0800

    refactor

--- argument_parser.py
+++ argument_parser.py
@@ -3,7 +3,7 @@ import time
 
 from load_config import node_id, channel, key, root_topic, destination_id
 from utils import validate_lat_lon_alt
-from tx_message_handler import create_position_payload, create_text_payload
+from tx_message_handler import create_position_payload, create_text_payload, publish_message
 
 def get_args():
     """Define and parse command-line arguments."""
@@ -18,5 +18,39 @@ def get_args():
 
 
 
+def handle_args(client, args):
+    
+    if args.message:
+        publish_message(
+            create_text_payload,
+            client=client,
+            node_id=node_id,
+            destination_id=destination_id,
+            channel=channel,
+            key=key,
+            message_text=args.message
+        )
+        time.sleep(3)
 
+    # Handle position arguments
+    if args.lat or args.lon:
+        parser, args = get_args()
+        validate_lat_lon_alt(parser, args)
+        lat = args.lat
+        lon = args.lon
+        alt = args.alt if args.alt else 0
+
+        publish_message(
+            create_position_payload,
+            client=client,
+            node_id=node_id,
+            destination_id=destination_id,
+            channel=channel,
+            key=key,
+            lat=lat,
+            lon=lon,
+            alt=alt
+        )
+        print(f"Sending Position Packet to {str(destination_id)}")
+        time.sleep(3)
 
--- main.py
+++ main.py
@@ -3,71 +3,26 @@
 Powered by Meshtastic™ https://meshtastic.org/
 """
 
-import random
 import time
-
-from meshtastic import BROADCAST_NUM
 import paho.mqtt.client as mqtt
+from meshtastic import BROADCAST_NUM
 
 from tx_message_handler import create_nodeinfo_payload, create_position_payload, create_text_payload, publish_message
-from utils import validate_lat_lon_alt
 from rx_message_handler import on_message
 from load_config import (
-    mqtt_broker, mqtt_port, mqtt_username, mqtt_password,
-    root_topic, channel, key, node_id, node_short_name,
+    mqtt_broker, mqtt_port, mqtt_username, mqtt_password, channel, 
+    key, node_id, node_short_name,
     node_long_name, lat, lon, alt, node_hw_model, destination_id
 )
 from mqtt import connect_mqtt, on_connect, on_disconnect
-from argument_parser import get_args
+from argument_parser import get_args, handle_args
 
 # Configuration
 auto_reconnect = True
 auto_reconnect_delay = 1  # seconds
-
 stay_connected = False
 
-# Parse arguments
 parser, args = get_args()
-
-
-def handle_args(args):
-    if args.message:
-        publish_message(
-            create_text_payload,
-            client=client,
-            node_id=node_id,
-            destination_id=destination_id,
-            channel=channel,
-            key=key,
-            message_text=args.message
-        )
-        time.sleep(3)
-
-    # Handle position arguments
-    if args.lat or args.lon:
-        validate_lat_lon_alt(parser, args)
-        lat = args.lat
-        lon = args.lon
-        alt = args.alt if args.alt else 0
-
-        publish_message(
-            create_position_payload,
-            client=client,
-            node_id=node_id,
-            destination_id=destination_id,
-            channel=channel,
-            key=key,
-            lat=lat,
-            lon=lon,
-            alt=alt
-        )
-        print(f"Sending Position Packet to {str(destination_id)}")
-        time.sleep(3)
-
-
-
-############################
-# MQTT Client Setup
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="", clean_session=True, userdata=None)
 client.on_connect = on_connect
@@ -78,9 +33,8 @@ connect_mqtt(client, mqtt_broker, mqtt_port, mqtt_username, mqtt_password)
 client.loop_start()
 time.sleep(1)
 
-############################
-# Send initial node info payload
 
+# Send initial node info payload
 publish_message(
     create_nodeinfo_payload,
     client=client,
@@ -96,10 +50,7 @@ publish_message(
 
 time.sleep(3)
 
-handle_args(args)
-
-############################
-# Stay connected or disconnect
+handle_args(client, args)
 
 if not stay_connected:
     client.disconnect()
